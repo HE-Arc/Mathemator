37d888b6c95ed6b148e70899ba528dcdeae4c3a0
roxane.carraux <roxane.carraux@he-arc.ch>
2020-02-27 13:36:32 +0100

    add model student and exercise, migrations, seeding and admin view

--- mathematorProject/mathematorApp/admin.py
+++ mathematorProject/mathematorApp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Student
+from .models import Exercise
+
+
+admin.site.register(Student)
+admin.site.register(Exercise)
--- mathematorProject/mathematorApp/models.py
+++ mathematorProject/mathematorApp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Student(models.Model):
+    firstName = models.CharField(max_length=50)
+    lastName = models.CharField(max_length=50)
+    email = models.EmailField(max_length=100)
+    isTeacher = models.BooleanField()
+
+class Exercise(models.Model):
+    name = models.CharField(max_length=50)
+    objective = models.CharField(max_length=50)
+    theme = models.CharField(max_length=50)
+    branches = models.CharField(max_length=50)
+    year = models.CharField(max_length=50)
