6ad8a26fce0b7c9a34fc8684b759f692844d6215
roxane.carraux <roxane.carraux@he-arc.ch>
2020-02-27 14:00:54 +0100

    add models exercicesRequirement and exerciseDone : migrations,seeding and admin view

--- mathematorProject/mathematorApp/admin.py
+++ mathematorProject/mathematorApp/admin.py
@@ -3,7 +3,10 @@ from django.contrib import admin
 # Register your models here.
 from .models import Student
 from .models import Exercise
-
+from .models import ExerciseRequirement
+from .models import ExerciseDone
 
 admin.site.register(Student)
 admin.site.register(Exercise)
+admin.site.register(ExerciseRequirement)
+admin.site.register(ExerciseDone)
--- mathematorProject/mathematorApp/models.py
+++ mathematorProject/mathematorApp/models.py
@@ -7,6 +7,8 @@ class Student(models.Model):
     lastName = models.CharField(max_length=50)
     email = models.EmailField(max_length=100)
     isTeacher = models.BooleanField()
+    relationExerciseDone =  models.ManyToManyField('Exercise', through='ExerciseDone', symmetrical=False, related_name='related_to_exercisedone')
+
 
 class Exercise(models.Model):
     name = models.CharField(max_length=50)
@@ -14,3 +16,15 @@ class Exercise(models.Model):
     theme = models.CharField(max_length=50)
     branches = models.CharField(max_length=50)
     year = models.CharField(max_length=50)
+    relationExerciseRequirement = models.ManyToManyField('self', through='ExerciseRequirement', symmetrical=False, related_name='related_to_exerciserequirement')
+    relationExerciseDone =  models.ManyToManyField('Student', through='ExerciseDone', symmetrical=False, related_name='related_to_exercisedone')
+
+class ExerciseRequirement(models.Model):
+    idExercise = models.ForeignKey(Exercise, related_name='fromExercise', on_delete=models.CASCADE)
+    idExerciseRequirement = models.ForeignKey(Exercise, related_name='toExercise', on_delete=models.CASCADE)
+
+class ExerciseDone(models.Model):
+    idStudent = models.ForeignKey(Student, on_delete=models.CASCADE)
+    idExercise =  models.ForeignKey(Exercise,  on_delete=models.CASCADE)
+    nbWrong = models.IntegerField()
+    nbRight = models.IntegerField()
