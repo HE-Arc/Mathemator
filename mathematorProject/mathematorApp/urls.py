545356b77519cda99d990d554e9b1ccb14f6f5e2
Claude Edwin <edwin.claude@he-arc.ch>
2020-03-04 16:26:42 +0100

    Ajout page index.html

--- mathematorProject/mathematorApp/urls.py
+++ mathematorProject/mathematorApp/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('profile', views.profile, name='profile'),
+    path('logout', views.logout, name='logout'),
 ]
--- mathematorProject/mathematorApp/views.py
+++ mathematorProject/mathematorApp/views.py
@@ -2,4 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the mathemator index.")
+    return render(request, "index.html", {})
+
+def profile(request):
+    return render(request, "profile.html", {})
+
+def logout(request):
+    return HttpResponse("Logout");
