3da3364a3ce68cbda8698ca212238db70a35f518
loic.jurasz <loic.jurasz@he-arc.ch>
2020-03-04 16:07:02 +0100

    adding login

--- mathematorProject/mathematorApp/urls.py
+++ mathematorProject/mathematorApp/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.conf.urls import include
 
 from . import views
 
 urlpatterns = [
+    path('accounts/', include('django.contrib.auth.urls')),
     path('', views.index, name='index'),
 ]
--- mathematorProject/mathematorApp/views.py
+++ mathematorProject/mathematorApp/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def index(request):
-    return HttpResponse("Hello, world. You're at the mathemator index.")
+    return render(request, "index.html", {})
+
+
+def login(request):
+    print("hello")
+    if request.user.is_authenticated:
+        return render(request, "index.html", {})
+    else:
+        return login(request)
+
+def login_view(request):
+    if request.method == 'POST':
+        print("error, no post")
+    else:
+        form = AuthentificationForm()
+    return render(request,'mathemator/login.html',{'form':form})
