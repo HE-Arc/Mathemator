bafa88c1cf2fe7771fdca75a5cc0636c61e10775
Claude Edwin <edwin.claude@he-arc.ch>
2020-03-18 18:43:40 +0100

    Add navigation bar

--- mathematorProject/mathematorApp/urls.py
+++ mathematorProject/mathematorApp/urls.py
@@ -5,6 +5,6 @@ from . import views
 
 urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
-    path('', views.index, name='index'),
     path('profile', views.profile, name='profile'),
+    path('', views.ExerciseView.as_view(), name='index'),
 ]
--- mathematorProject/mathematorApp/views.py
+++ mathematorProject/mathematorApp/views.py
@@ -2,10 +2,21 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.decorators import login_required
+from django.views import generic, View
+
+from .models import Exercise
 
 @login_required
 def index(request):
     return render(request, "index.html", {})
+
+class ExerciseView(generic.TemplateView):
+    template_name = "index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['exercises'] = Exercise.objects.all()
+        return context
 
 def profile(request):
     return render(request, "profile.html", {})
