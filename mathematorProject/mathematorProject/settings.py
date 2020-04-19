9a52da9b1ac53f969ae0e873d260e9462ce04e51
RoxaneCarraux <roxane.carraux@he-arc.ch>
2020-04-19 18:06:25 +0200

    try to fix style.css error in console

--- mathematorProject/mathematorProject/settings.py
+++ mathematorProject/mathematorProject/settings.py
@@ -18,9 +18,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
