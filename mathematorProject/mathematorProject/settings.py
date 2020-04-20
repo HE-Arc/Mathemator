398388d9f1a7ca7c2f782d9bfaee1101b81d5731
RoxaneCarraux <roxane.carraux@he-arc.ch>
2020-04-20 09:08:04 +0200

    fix style.css

--- mathematorProject/mathematorProject/settings.py
+++ mathematorProject/mathematorProject/settings.py
@@ -17,10 +17,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 #STATIC_ROOT = os.path.join(BASE_DIR,"static")
-STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR,"static"),
+    os.path.normpath(os.path.join(BASE_DIR, "static")),
 )
+
+MEDIA_URL =  'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, MEDIA_URL)
 
 
 # Quick-start development settings - unsuitable for production
