194f06e2ba13f7a50d2946c2e81b0b66f9a4e167
RoxaneCarraux <roxane.carraux@he-arc.ch>
2020-04-20 09:01:14 +0200

    fix style.css

--- mathematorProject/mathematorProject/settings.py
+++ mathematorProject/mathematorProject/settings.py
@@ -16,7 +16,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
-STATIC_ROOT = os.path.join(BASE_DIR,"static")
+#STATIC_ROOT = os.path.join(BASE_DIR,"static")
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR,"static"),
