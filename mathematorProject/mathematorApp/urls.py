a0ef3cc6b3516c1eba917d7486f14fc029f6db82
RoxaneCarraux <roxane.carraux@he-arc.ch>
2020-04-20 08:54:44 +0200

    fix style.css

--- mathematorProject/mathematorApp/urls.py
+++ mathematorProject/mathematorApp/urls.py
@@ -8,6 +8,7 @@ He-Arc
 from django.urls import path
 from django.conf.urls import include
 from . import views
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
